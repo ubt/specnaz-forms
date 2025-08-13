425f88fb323d1ed2c783c40186c0e1370e2c617a
наркопритон <fuqmlf@gmail.com>
2025-08-14 01:13:38 +0300

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1>Notion Skill Review</h1>
+      <p>Админ-страница генерации ссылок: <a href="/admin">/admin</a></p>
+      <p>Формы для оценок открываются по адресам вида: <code>/form/&lt;token&gt;</code></p>
+    </main>
+  );
+}
