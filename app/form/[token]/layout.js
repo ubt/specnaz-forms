f2203d1b55cf6512e9196ac5e98214018fef8e02
наркопритон <fuqmlf@gmail.com>
2025-08-16 13:15:36 +0300

    Update layout.js

--- app/form/[token]/layout.js
+++ app/form/[token]/layout.js
@@ -1,5 +1,9 @@
 // Конфиг роута на Edge Runtime для страницы /form/[token]
 export const runtime = 'edge';
+// Отключаем статическую генерацию для динамических страниц
+export const dynamic = 'force-dynamic';
+// Отключаем статический экспорт для этого маршрута
+export const dynamicParams = true;
 
 export default function FormLayout({ children }) {
   return <>{children}</>;
