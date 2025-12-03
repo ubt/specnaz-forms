c4180909943300948716e9f64390d9119e0ada5d
наркопритон <fuqmlf@gmail.com>
2025-12-03 04:41:26 +0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -20,7 +20,7 @@ export default function Home() {
 <iframe
 	width="1120"
 	height="630"
-	src="https://www.youtube.com/embed/PkT0PJwy8mI?si=QH5SZLRUnedZAZJk?autoplay=1"
+	src="https://www.youtube.com/embed/PkT0PJwy8mI?si=QH5SZLRUnedZAZJk"
 	title="YouTube video player"
 	frameborder="0"
 	allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
