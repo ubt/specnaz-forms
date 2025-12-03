11616e6c158a2b182f83c5196cb514cc241045c1
наркопритон <fuqmlf@gmail.com>
2025-12-03 04:18:45 +0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,14 +1,14 @@
 export default function Home() {
   return (
-    <main style={{ 
-      padding: 32, 
-      maxWidth: 800, 
+    <main style={{
+      padding: 32,
+      maxWidth: 800,
       margin: '0 auto',
       fontFamily: 'system-ui, sans-serif'
     }}>
       <div style={{ textAlign: 'center', marginBottom: 48 }}>
-        <h1 style={{ 
-          fontSize: 36, 
+        <h1 style={{
+          fontSize: 36,
           color: '#2c3e50',
           marginBottom: 16,
           fontWeight: 700
@@ -20,14 +20,15 @@ export default function Home() {
         </p>
       </div>
 
-<iframe 
-	width="1120" 
-	height="630" 	
-	src="https://www.youtube.com/embed/PkT0PJwy8mI?si=QH5SZLRUnedZAZJk" 
-	title="YouTube video player" 
-	frameborder="0" 
-	allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-	referrerpolicy="strict-origin-when-cross-origin" 
+<iframe
+	width="1120"
+	height="630"
+	src="https://www.youtube.com/embed/PkT0PJwy8mI?si=QH5SZLRUnedZAZJk"
+	title="YouTube video player"
+	frameborder="0"
+	allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+	referrerpolicy="strict-origin-when-cross-origin"
+	style ="margin:auto"
 	allowfullscreen="true"
 </iframe>
     </main>
